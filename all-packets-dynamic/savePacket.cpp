255abcba34817e10ee68e9e5f3246ef14a2faeca
KarimCoding <karim.alghoul@hotmail.com>
2015-11-04 12:27:46 -0500

    Create savePacket.cpp

---
+++ all-packets-dynamic/savePacket.cpp
@@ -0,0 +1,31 @@
+//
+//this function check if the checkSum is correct and save the packet to the file
+//
+
+#include <SerialStream.h>
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <unistd.h>
+#include <cstdlib>
+#include <iterator>
+
+using namespace std;
+
+
+void savePacket(unsigned char sumCAC,unsigned char saveArray[], int k)
+{
+  std::fstream file; // for saving the data on a file
+  file.open("test.txt", std::fstream::app); // open the file that you want to save data in it
+  if ( int(sumCAC) == int(saveArray[(k-1)]))
+    {
+      for(int x=0; x <k ; x++)
+	{
+	  file << std::setw(2) << std::setfill('0')<< std::hex <<(int)(saveArray[x])<<" ";
+	}
+      file << "       " ;
+    }
+  
+  file<<endl;
+  file.close();
+}
